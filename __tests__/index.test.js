e5069d4c96de915240e171aff2001aa7e6c8ad9c
Chanon Roy <chanonroy@gmail.com>
2019-04-15 21:41:26 +0100

    test(jest): add jest tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,21 @@
+const isChanon = require('../index.js');
+
+test('is TRUE with "Chanon"', () => {
+  expect(isChanon('Chanon')).toBe(true);
+});
+
+test('is TRUE with lowercase "chanon"', () => {
+  expect(isChanon('chanon')).toBe(true);
+});
+
+test('is TRUE with other spacing and "chanon"', () => {
+  expect(isChanon(' chanon ')).toBe(true);
+});
+
+test('is FALSE with other string', () => {
+  expect(isChanon('john')).toBe(false);
+});
+
+test('is FALSE with number', () => {
+  expect(isChanon(123)).toBe(false);
+});
